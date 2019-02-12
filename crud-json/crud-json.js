4fd13dfb76efb168dd34a61ed4037b28ff70b540
Risyandi <Risyandi.94@gmail.com>
2019-02-12 17:36:56 +0700

    adding crud json

---
+++ crud-json/crud-json.js
@@ -0,0 +1,28 @@
+var family = {
+    "Risyandi": {
+        "name": "Risyandi white",
+        "gender": "male",
+        "house": "jatinangor"
+    },
+    "Angela": {
+        "name": "angela white",
+        "gender": "female",
+        "house": "india"
+    },
+    "Mariska": {
+        "name": "mariska white",
+        "gender": "female",
+        "house": "australia"
+    },
+}
+
+// create add character
+var characterObject = {
+    "name": "Bella Ramsey",
+    "gender": "Female",
+    "house": "Mormonts"
+}
+var character = "Lyanna";
+family[character] = characterObject;
+console.log(family);
+
