e551fd00d8710c38307017b9ea86e5ba036b42e7
risyandi94 <Risyandi@gvmnetworks.com>
2019-03-01 23:38:11 +0700

    adding filtering sentences

---
+++ filtering-sentences/filtering-sentences.js
@@ -0,0 +1,28 @@
+var sentence = 'dasar kau cebong dan kau kampret asem',
+    common = 'cebong, kampret';
+
+function getUncommon(sentence, common) {
+    var wordArr = sentence.match(/\w+/g),
+        commonObj = {},
+        uncommonArr = [],
+        word, i;
+    
+    common = common.split(',');
+    for ( i = 0; i < common.length; i++ ) {
+        commonObj[ common[i].trim() ] = true;
+    }
+    
+    for ( i = 0; i < wordArr.length; i++ ) {
+        word = wordArr[i].trim().toLowerCase();
+        if ( !commonObj[word] ) {
+            uncommonArr.push(word);
+        }
+    }
+    var cek = uncommonArr.join();
+    console.log(cek);
+    
+    return uncommonArr;
+}
+
+// document.write( getUncommon(sentence, common) );
+getUncommon(sentence, common);
