cb5ec2bb67d724e5554486bb1c5aed7f82a39589
risyandi <risyandi.94@gmail.com>
2019-03-21 13:48:59 +0700

    update fetch api

--- fetch-fake-api/fetch-fake-api.js
+++ fetch-fake-api/fetch-fake-api.js
@@ -1,20 +1,20 @@
 // var linkTodo = "jsonplaceholder.typicode.com/todos/1";
 var linkPhoto = "jsonplaceholder.typicode.com/photos";
+var submitButton = document.getElementById("submit-btn");
 
 // version arrow function
 function fetchData(linkPhoto) {
     fetch('https://' + linkPhoto)
         .then(response => response.json())
         .then(data => {
-            var nextIndex = 1,
-                start = 0,
+            var start = 0,
                 end = 5;
             // console.log(data.length, ":value of data length");
             for (let index = start; index < end; index++) {
                 // console.log(data[index]);
                 var element = '';
                 element += '<a href="https://' + 'populify.com' + '/">'
-                element += '<img style="width: 200px; height: 200px;" src="' + data[index].url + '" alt="images-' + nextIndex++ + '" srcset="' + data[index].url + '"></img>'
+                element += '<img style="width: 200px; height: 200px;" src="' + data[index].url + '" alt="images-' + index + '" srcset="' + data[index].url + '"></img>'
                 element += '</a>';
                 // append element
                 document.getElementById("draw-html").innerHTML += element;
@@ -23,6 +23,6 @@ function fetchData(linkPhoto) {
 }
 fetchData(linkPhoto);
 
-document.getElementById("submit-btn").addEventListener("click", function () {
+submitButton.addEventListener("click", function () {
     fetchData(linkPhoto);
 }, false);
