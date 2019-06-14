9f9a8bd8296059ee657fccd68918bf2f34fdff16
risyandi <risyandi.94@gmail.com>
2019-06-14 17:51:31 +0700

    adding getter and setter on arrow js

--- arrow-classic-function/arrow-classic-function.js
+++ arrow-classic-function/arrow-classic-function.js
@@ -19,13 +19,13 @@ let Obj = {
     myFunc: function () {
         console.log(this.myVar);
     },
-    myFunc2: function() {
+    myFunc2: function () {
         console.log("hallo");
 
         // to access scope 
-        let self =  this
+        let self = this;
 
-        setTimeout(function() {
+        setTimeout(function () {
             console.log(this.myVar); // output: undefined -> because outside scope
             console.log(self.myVar); // output: lorem ipsum -> if adding var self
         }, 1000);
@@ -35,4 +35,47 @@ Obj.myFunc(); // output: lorem ipsum
 Obj.myFunc2(); // output: hallo
 
 // testing scope in arrow function
-// myFunc2(); // output: error "myFunc2 is not defined" -> because not in the scope
+// myFunc2();  // output: error "myFunc2 is not defined" -> because not in the scope
+
+/**
+ * rest parameter 
+ */
+function sum(...theArgs) {
+    return theArgs.reduce((previous, current) => {
+        return previous + current;
+    });
+}
+console.log(sum(1, 2, 3)); // output: 6
+console.log(sum(1, 2, 3, 4)); // output: 10
+
+
+/**
+ * getter
+ */
+var Obja = {
+    log: ['A', 'B', 'C'],
+    get latest() {
+        if (this.log.length == 0) {
+            return undefined;
+        }
+        return this.log[this.log.length - 1];
+    }
+}
+
+console.log(Obja.latest); // output: 'C'
+
+
+/**
+ * setter
+ */
+var language = {
+    set current(name) {
+        this.log.push(name);
+    },
+    log: []
+}
+
+language.current = 'EN';
+language.current = 'FA';
+
+console.log(language.log); // output: array['EN', 'FA']
