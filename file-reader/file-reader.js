876ef71c61c5cc1a64d1230b947844705b010c32
risyandi <risyandi.94@gmail.com>
2019-07-06 21:04:22 +0700

    update file js

--- file-reader/file-reader.js
+++ file-reader/file-reader.js
@@ -1,19 +1,51 @@
-// how to get file uploading by you
+/**
+ * how to get file uploading
+ * using constructor a new fileReader.
+ *  */
 var openFile = function (event) {
     var input = event.target;
-        console.log(input, ":value input");
-    
+    console.log(input, ":value input");
+
     var reader = new FileReader();
-        console.log(reader, ":value reader");
-    
+    console.log(reader, ":value reader");
+
     reader.onload = function () {
         var dataUrl = reader.result;
-            console.log(dataUrl, ":value dataUrl");
-        
-        var output = document.getElementById('output');
-            console.log(output, ":value output");
-        
+        console.log(dataUrl, ":value dataUrl");
+
+        var output = document.getElementById('output-images');
+        console.log(output, ":value output");
+
         output.src = dataUrl;
     };
     reader.readAsDataURL(input.files[0]);
 };
+
+
+/**
+ * instance properties
+ * to get error encountered during load
+ */
+
+var openFileError = function (event) {
+    var input = event.target;
+    console.log(input, ":value input");
+    
+    var reader = new FileReader();
+    console.log(reader, ":value reader");
+    
+    /** read file when onload start */
+    reader.onloadstart = function () {
+        reader.abort();
+    }
+    
+    /** read file when onload end and give a message of error */
+    reader.onloadend = function () {
+        console.log(reader.error.message);
+        var res = document.getElementById('response-error');
+        res.innerText = reader.error.message;
+    }
+
+    /** read file data as url */
+    reader.readAsDataURL(input.files[0]);
+}
